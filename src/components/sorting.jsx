44fcc613c59d0401fd133235ff9a85b42535ed89
Marat Assimbayev <chesterfild-9@mail.ru>
2022-06-02 00:03:10 +0600

    pagination

--- src/components/sorting.jsx
+++ src/components/sorting.jsx
@@ -1,66 +1,64 @@
 import React, { useState } from "react";
 
 const Sorting = ({ value, onChangeSort }) => {
-  // состояние для popup
-  const [open, setOpen] = useState(false);
+	// состояние для popup
+	const [open, setOpen] = useState(false);
 
-  // меню сортировки
-  const menuSort = [
-    // с помощью минуса создаем уникальность
-    { name: "популярности (DESC)", sortProperty: "rating" },
-    { name: "популярности (ASC)", sortProperty: "-rating" },
-    { name: "цене (DESC)", sortProperty: "price" },
-    { name: "цене (ASC)", sortProperty: "-price" },
-    { name: "алфавиту (DESC)", sortProperty: "title" },
-    { name: "алфавиту (ASC)", sortProperty: "-title" },
-  ];
+	// меню сортировки
+	const menuSort = [
+		// с помощью минуса создаем уникальность
+		{ name: "популярности (DESC)", sortProperty: "rating" },
+		{ name: "популярности (ASC)", sortProperty: "-rating" },
+		{ name: "цене (DESC)", sortProperty: "price" },
+		{ name: "цене (ASC)", sortProperty: "-price" },
+		{ name: "алфавиту (DESC)", sortProperty: "title" },
+		{ name: "алфавиту (ASC)", sortProperty: "-title" },
+	];
 
-  // функция выбора свойства сортировки
-  const handleSelected = (i) => {
-    onChangeSort(i);
-    setOpen();
-    console.log("что это", i);
-  };
+	// функция выбора свойства сортировки
+	const handleSelected = (i) => {
+		onChangeSort(i);
+		setOpen();
+		console.log("что это", i);
+	};
 
-  return (
-    <div className="sort">
-      <div className="sort__label">
-        <svg
-          width="10"
-          height="6"
-          viewBox="0 0 10 6"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-            fill="#2C2C2C"
-          />
-        </svg>
-        <b>Сортировка по:</b>
-        <span onClick={() => setOpen(!open)}>{value.name}</span>
-      </div>
-      {open && (
-        <div className="sort__popup">
-          <ul>
-            {menuSort.map((obj, i) => (
-              <li
-                onClick={() => {
-                  handleSelected(obj);
-                }}
-                className={
-                  value.sortProperty === obj.sortProperty ? "active" : ""
-                }
-                key={i}
-              >
-                {obj.name}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className="sort">
+			<div className="sort__label">
+				<svg
+					width="10"
+					height="6"
+					viewBox="0 0 10 6"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg">
+					<path
+						d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
+						fill="#2C2C2C"
+					/>
+				</svg>
+				<b>Сортировка по:</b>
+				<span onClick={() => setOpen(!open)}>{value.name}</span>
+			</div>
+			{open && (
+				<div className="sort__popup">
+					<ul>
+						{menuSort.map((obj, i) => (
+							<li
+								onClick={() => {
+									handleSelected(obj);
+								}}
+								className={
+									value.sortProperty === obj.sortProperty ? "active" : ""
+								}
+								key={i}>
+								{obj.name}
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default Sorting;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,56 +4,56 @@ import PizzaBlock from "../components/PizzaBlock";
 import Categories from "../components/Categories";
 import Sorting from "../components/Sorting";
 import Skeleton from "../components/PizzaBlock/Skeleton";
+import Pagination from "../components/Pagination";
 
 const Home = ({ searchValue }) => {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [categoryId, setCategoryId] = useState(0);
-  const [sortType, setSortType] = useState({
-    name: "популярности",
-    sortProperty: "rating",
-  });
+	const [items, setItems] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [categoryId, setCategoryId] = useState(0);
+	const [sortType, setSortType] = useState({
+		name: "популярности",
+		sortProperty: "rating",
+	});
 
-  console.log(searchValue.toLowerCase(), "dsdsds");
+	const sortBy = sortType.sortProperty.replace("-", "");
+	const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
+	const category = categoryId > 0 ? `category=${categoryId}` : "";
+	const search = searchValue ? `&search=${searchValue}` : "";
 
-  const sortBy = sortType.sortProperty.replace("-", "");
-  const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
-  const category = categoryId > 0 ? `category=${categoryId}` : "";
+	useEffect(() => {
+		setIsLoading(true);
+		fetch(
+			`https://628baebb667aea3a3e34800b.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}`
+		)
+			.then((res) => res.json())
+			.then((json) => {
+				setItems(json);
+				setIsLoading(false);
+			});
+		window.scrollTo(0, 0);
+	}, [categoryId, sortType, searchValue]);
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      `https://628baebb667aea3a3e34800b.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        setItems(json);
-        setIsLoading(false);
-      });
-  }, [categoryId, sortType]);
+	const skeletons = [...new Array(6)].map((_, index) => (
+		<Skeleton key={index} />
+	));
+	const pizzas = items.map((obj) => <PizzaBlock {...obj} key={obj.id} />);
+	/* 	.filter((item) =>
+	item.title.toLowerCase().includes(searchValue.toLowerCase()) */
 
-  const skeletons = [...new Array(6)].map((_, index) => (
-    <Skeleton key={index} />
-  ));
-  const pizzas = items
-    .filter((item) =>
-      item.title.toLowerCase().includes(searchValue.toLowerCase())
-    )
-    .map((obj) => <PizzaBlock {...obj} key={obj.id} />);
-
-  return (
-    <div className="container">
-      <div className="content__top">
-        <Categories
-          value={categoryId}
-          onChangeCategory={(i) => setCategoryId(i)}
-        />
-        <Sorting value={sortType} onChangeSort={(i) => setSortType(i)} />
-      </div>
-      <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
-    </div>
-  );
+	return (
+		<div className="container">
+			<div className="content__top">
+				<Categories
+					value={categoryId}
+					onChangeCategory={(i) => setCategoryId(i)}
+				/>
+				<Sorting value={sortType} onChangeSort={(i) => setSortType(i)} />
+			</div>
+			<h2 className="content__title">Все пиццы</h2>
+			<div className="content__items">{isLoading ? skeletons : pizzas}</div>
+			<Pagination />
+		</div>
+	);
 };
 
 export default Home;
