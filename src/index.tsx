acdd948bcbc068904cfc09948b73a0ed1a2fc018
Марат <D:\Program Files\The Bat!\Mail>
2022-07-14 18:23:48 +0600

    dd

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
 import App from "./App";
 
 ReactDOM.render(
-	<HashRouter>
+	<BrowserRouter>
 		<Provider store={store}>
 			<App />
 		</Provider>
-	</HashRouter>,
+	</BrowserRouter>,
 	document.getElementById("root")
 );
