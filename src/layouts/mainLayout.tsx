13d8436321a8618735cf941de864dd8883dc8ce5
Марат <D:\Program Files\The Bat!\Mail>
2022-07-15 12:06:40 +0600

    dsds

--- src/layouts/mainLayout.tsx
+++ src/layouts/mainLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 import Header from "../components/header";
 
-// создали для того чтобы вынести разметку из  App.tsx
+// создали для того чтобы вынести разметку из  App.tsx интереснопше
 const MainLayout: React.FC = () => {
 	return (
 		<div className="wrapper">
